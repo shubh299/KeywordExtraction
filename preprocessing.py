55f2411cd7538636761a4544df534c73bdbb4340
Shubham Singh <singhshubham2909@gmail.com>
2019-03-04 18:45:44 +0530

    Added CD to stop tags for numbers
    Commented printing of candidate keywords

--- preprocessing.py
+++ preprocessing.py
@@ -24,7 +24,7 @@ def get_wordnet_pos(treebank_tag):
 
 def preprocess(input_text):
 	input_text=input_text.lower()
-	stop_tags=['CC','DT','EX','IN','LS','MD','PDT','PRP','PRP$','TO','UH','WDT','WP','WP$','WRB','VB','VBD','VBG','VBN','VBP',',','.',':',';','(',')','[',']','{','}']
+	stop_tags=['CC','CD','DT','EX','IN','LS','MD','PDT','PRP','PRP$','TO','UH','WDT','WP','WP$','WRB','VB','VBD','VBG','VBN','VBP',',','.',':',';','(',')','[',']','{','}']
 	#stop_words=list(stopwords.words('english'))+['also','usually','however','enable','give','type','use','mixed','consider']
 	stop_words=[word.rstrip("\n") for word in open("stopWords.txt",mode="r")]
 	#stop_words=['enable','i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'be', 'do', 'until', 'about', 'against', 'between', 'into', 'through', 'during', 'again', 'further', 'then', 'once', 'here', 'there', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't", 'give', 'type', 'use', 'mixed', 'consider']
@@ -51,7 +51,7 @@ def preprocess(input_text):
 	candidate_keyword_list_final=list(filter(lambda a: a!='',candidate_keyword_list_final))
 	for index in range(len(candidate_keyword_list_final)):
 		candidate_keyword_list_final[index]=candidate_keyword_list_final[index].strip(" ")
-	print(candidate_keyword_list_final)
+	#print(candidate_keyword_list_final)
 	return candidate_keyword_list_final
 
 
